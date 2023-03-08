c6b6f3d766d4638aed7089c317d9db3cd708bad5
Khairulhub <iubat21103033@gmail.com>
2023-03-08 19:48:05 +0600

    portfolio api all call and single portfolio add api done

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT||5000;
 
-
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+// make connection with mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rj3brpj.mongodb.net/?retryWrites=true&w=majority`;
+// make connection with mongodb 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect();
+        // console.log('connected');
+        const portfolioCollection = client.db('portfolio').collection('portfolio-card');
+
+
+        app.get('/portfolio', async (req, res) => {
+            const cursor = portfolioCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/portfolio', async (req, res) => {
+            const {portfolio} = req.body;
+            
+            const result = await portfolioCollection.insertOne(portfolio);
+            res.json(result);
+        })
+        
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
+
+console.log(uri);
+
 
 // database connection
 app.get('/', (req, res) => {
